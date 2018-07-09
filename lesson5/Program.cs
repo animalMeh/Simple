a41fd7f3e703cac06132a7c2e28ae5434a68ed95
dasha <dasha@DESKTOP-R75H9K8>
2018-07-09 10:51:34 +0300

    Add project files.

---
+++ lesson5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Channel c1 = new Channel("ictv", 12);
+            Channel c2 = new Channel("novii", 22);
+            Channel c3 = new Channel("russia 1", 150);
+            Channel c4 = new Channel("animal planet", 66);
+
+            TV home = new TV("triolan", c1, c2, c3, c4);
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.NextChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.NextChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.NextChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.NextChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.NextChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number + "\n Previous: ");
+
+            home.PreviousChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.PreviousChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.PreviousChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.PreviousChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number);
+            home.PreviousChannel();
+            Console.WriteLine(" " + home.CurrentChannel.Name + ",number: " + home.CurrentChannel.Number + "\n Offering: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+           home.GoToChannel(number);
+            Console.WriteLine(home.CurrentChannel.Name);
+
+        }
+    }
+}
---
+++ lesson5/TV.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    class TV
+    {
+        private Channel[] Channels;
+        public Channel CurrentChannel;
+        public string Provider { get; private set; }
+
+        public TV(string _provider , params Channel[] _pep)
+        {
+            Provider = _provider;
+            AddChanels(_pep);
+            CurrentChannel = Channels[0];
+        }
+
+
+        public  void AddChanels(params Channel[] _pep)
+        {
+            int i = 0;
+            Channels = new Channel[_pep.Count()];
+            foreach(Channel c in _pep)
+            {
+                Channels[i++] = c;
+            }
+        }
+        
+        public void NextChannel()
+        {
+            if (CurrentChannel == Channels[Channels.GetLength(0) - 1])
+            {
+                CurrentChannel = Channels[0];
+                return;
+            }
+            for (int i = 0; i < Channels.GetLength(0) - 1; i++ )
+            {
+               
+                if (CurrentChannel.Equals(Channels[i]))
+                {
+                    CurrentChannel = Channels[i + 1];
+                    return;
+                }   
+             }
+        }
+        public void PreviousChannel()
+        {
+            if (CurrentChannel == Channels[0])
+            {
+                CurrentChannel = Channels[Channels.GetLength(0) - 1];
+                return;
+            }
+
+            for (int i = 1; i < Channels.GetLength(0) ; i++)
+            {
+               
+                if (CurrentChannel.Equals(Channels[i]))
+                {
+                    CurrentChannel = Channels[i - 1];
+                    return;
+                }
+            }
+        }
+        public void GoToChannel(int Number)
+        {
+            if (CurrentChannel.Number.Equals(Number)) return;
+            foreach (Channel ch in Channels)
+                if (ch.Number.Equals(Number))
+                    CurrentChannel = ch;
+        }
+
+    }
+    class Channel
+    {
+        public Channel(string _name , int _num)
+        {
+            Name = _name;
+            Number = _num;
+        }
+        public string Name
+        {
+            get;set;
+        }
+        public int Number
+        { get; set; }
+    }
+}
